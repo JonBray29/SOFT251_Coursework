ac6d73de4261dd3303ddf1337d903b0b473b1cd1
Jonathon Bray <jonathon.bray@students.plymouth.ac.uk>
2018-12-16 13:58:13 +0000

    Populated systemUsers class

--- PatientManagementSystemLibary/src/PatientSystem/SystemUsers.java
+++ PatientManagementSystemLibary/src/PatientSystem/SystemUsers.java
@@ -10,5 +10,60 @@ package PatientSystem;
  * @author Jonbr
  */
 public abstract class SystemUsers {
+    protected String userId;
+    protected String firstName;
+    protected String lastName;
+    protected String addressLine1;
+    protected String city;
+    protected String postcode;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddressLine1() {
+        return addressLine1;
+    }
+
+    public void setAddressLine1(String addressLine1) {
+        this.addressLine1 = addressLine1;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+    
     
 }
